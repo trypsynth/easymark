09483be3e692e878650f3e7f787d474a73697cd9
Quin <TheQuinbox@gmail.com>
2025-03-02 15:16:09 -0700

    Support passing a path on the command line.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![windows_subsystem = "windows"]
 use native_dialog::FileDialog;
 use std::{
+    env,
     fs::File,
     io::{Read, Write},
     process,
@@ -10,17 +11,24 @@ use std::{
 use tempfile::Builder;
 
 fn main() {
-    let path = FileDialog::new()
-        .add_filter("Markdown Files", &["md", "markdown"])
-        .add_filter("All Files", &["*"])
-        .show_open_single_file()
-        .unwrap();
-    let path = path
-        .ok_or_else(|| {
-            eprintln!("No file selected.");
-            process::exit(1);
-        })
-        .unwrap();
+    let args: Vec<String> = env::args().collect();
+    let path = if args.len() > 1 {
+        args[1].clone()
+    } else {
+        FileDialog::new()
+            .add_filter("Markdown Files", &["md", "markdown"])
+            .add_filter("All Files", &["*"])
+            .show_open_single_file()
+            .unwrap()
+            .ok_or_else(|| {
+                eprintln!("No file selected.");
+                process::exit(1);
+            })
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_string()
+    };
     let mut md_contents = String::new();
     File::open(&path).unwrap().read_to_string(&mut md_contents).unwrap();
     let mut html_file = Builder::new().suffix(".html").tempfile().unwrap();
